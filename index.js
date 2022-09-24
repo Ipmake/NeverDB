d8db684bf049083893660f2768189b39c50849e5
Ipmake123 <niko.denis727@gmail.com>
2022-09-24 15:11:08 +0200

    Added socket.io

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ const config = require('./config.json')
 const { Worker } = require('worker_threads')
 const chalk = require('chalk')
 const express = require('express')
-const socket = require('socket.io')
 const app = express()
 const admin = require('./admin')
-
-const ws = socket(config.port)
+const socket = require('./socket')
 
 app.use(express.json());
 
@@ -29,93 +27,6 @@ for (let i = 0; i < config.threads; i++) {
 }
 
 
-ws.on('connection', (socket) => {
-
-    socket.data = {
-        authenticated: false
-    }
-
-    socket.on('authenticate', (data) => {
-        if(data.secret === config.secret) {
-            socket.data.authenticated = true
-            socket.emit('authenticated')
-        } else {
-            socket.emit({ type: 'error', message: 'Invalid secret' })
-            socket.disconnect()
-        }
-    })
-
-
-
-    socket.on('get', ({db, key}) => {
-        if(!socket.data.authenticated) return socket.emit({ type: 'error', message: 'Not authenticated' }), socket.disconnect(), console.log(chalk.red(`[${new Date()}] Client not authenticated`))
-        const worker = workers[nextThread[0]]
-        switchThread(nextThread[0])
-        const id = Math.floor(Math.random() * 999999999)
-        
-        worker.postMessage({
-            type: 'request',
-            requestType: 'get',
-
-            db: db,
-            key: key,
-            id: id
-        })
-
-        new Promise((resolve, reject) => {
-            worker.on('message', (message) => {
-                if(message.type === 'response' && message.id === id) {
-                    resolve(message.data)
-                }
-            }
-            )
-        })
-        .then((message) => {
-            socket.emit('response_get', message)
-        })
-
-    })
-
-    socket.on('post', ({db, key, data}) => {
-        if(!socket.data.authenticated) return socket.emit({ type: 'error', message: 'Not authenticated' }), socket.disconnect(), console.log(chalk.red(`[${new Date()}] Client not authenticated`))
-        const worker = workers[nextThread[0]]
-        switchThread(nextThread[0])
-        const id = Math.floor(Math.random() * 999999999)
-        
-        worker.postMessage({
-            type: 'request',
-            requestType: 'post',
-
-            db: db,
-            key: key,
-            data: data,
-            id: id
-        })
-
-        new Promise((resolve, reject) => {
-            worker.on('message', (message) => {
-                if(message.type === 'response' && message.id === id) {
-                    resolve(message.data)
-                }
-            }
-            )
-        })
-        .then((message) => {
-            socket.emit('response_post', message)
-        })
-
-    })
-
-
-
-
-
-
-    socket.on('disconnect', () => {
-    })
-})
-
-
 
 
 
@@ -132,3 +43,5 @@ process.on('uncaughtException', (err) => {
 process.on('unhandledRejection', (err) => {
     console.log(chalk.red(err.stack))
 })
+
+new socket(config, {workers, nextThread, switchThread})
--- worker.js
+++ worker.js
@@ -17,7 +17,7 @@ parentPort.on('message', (message) => {
                             var data = JSON.parse(fs.readFileSync(`./${workerData.config.path}/${db}/${key}.nve`, 'utf8'))
                         }
                         catch(e) {
-                            return parentPort.postMessage({ type: 'response', id: message.id, data: null, err: "DB or Key not found" })
+                            return parentPort.postMessage({ type: 'response', id: message.id, data: null, err: "DB or Key not found on get" })
                         }
         
                         parentPort.postMessage({
@@ -37,7 +37,8 @@ parentPort.on('message', (message) => {
                             fs.writeFileSync(`./${workerData.config.path}/${db}/${key}.nve`, JSON.stringify(data))
                         }
                         catch(e) {
-                            return parentPort.postMessage({ type: 'response', id: message.id, data: null, err: "DB or Key not found" })
+                            console.log(e)
+                            return parentPort.postMessage({ type: 'response', id: message.id, data: null, err: "DB or Key not found on post" })
                         }
 
                         parentPort.postMessage({
@@ -48,6 +49,23 @@ parentPort.on('message', (message) => {
                         })
 
                     break;
+
+                    case 'delete':
+                        var { requestType, db, key } = message
+
+                        try{
+                            fs.unlinkSync(`./${workerData.config.path}/${db}/${key}.nve`)
+                        }
+                        catch(e) {
+                            return parentPort.postMessage({ type: 'response', id: message.id, data: null, err: "DB or Key not found on delete" })
+                        }
+
+                        parentPort.postMessage({
+                            type: 'response',
+                            requestType: requestType,
+                            data: null,
+                            id: message.id
+                        })
 
                 }
             })()
